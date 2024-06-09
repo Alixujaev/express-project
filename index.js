84a61e18e8cd3e6c95d5d87c46deff0daabe174a
islom <alixujaevislom@gmail.com>
2024-06-09 21:34:34 +0500

    feat: login auth

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express"
 import { create } from "express-handlebars"
 import AuthRoutes from "./src/routes/auth.js"
 import ProductsRoutes from "./src/routes/products.js"
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -13,12 +16,28 @@ const hbs = create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './src');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
-
 const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+
+const connectApp = () => {
+  try {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }, () => console.log("Database connected"))
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+connectApp()
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import User from "../models/User.js"
+import bcrypt from "bcrypt"
 const router = Router();
 
 router.get("/login", (req, res) => {
@@ -8,6 +9,37 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("register")
+})
+
+
+router.post("/login", async(req, res) => {
+  const existUser = await User.findOne({email: req.body.email})
+
+  if(!existUser){
+    console.log("User not found!");
+
+    return
+  }
+
+  const isEqualPass = await bcrypt.compare(req.body.password, existUser.password)
+
+  if(!isEqualPass){
+    console.log("Password is not correct!");
+    return
+  }
+
+  console.log(existUser);
+  res.redirect("/")
+})
+
+router.post("/register", async (req, res) => {
+  const hashedPass = await bcrypt.hash(req.body.password, 10)
+  const userData = await User.create({
+    ...req.body,
+    password: hashedPass
+  })
+
+  res.redirect("/")
 })
 
 export default router
